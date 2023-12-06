960c5d76bde24379b718992e627e201b91d5ebdb
micbuffa <micbuffa@gmail.com>
2023-12-06 08:41:39 +0100

    refactor comportement arrival avec deux démos

--- 3-Arrival/sketch.js
+++ 3-Arrival/sketch.js
@@ -1,16 +1,82 @@
 let vehicle;
 let vehicles = [];
+let cibles = [];
+let demo = "snake";
 
 function setup() {
   createCanvas(800, 800);
 
-  let nbVehicles = 2;
+  /* POUR LE SNAKE 
+  let nbVehicles = 20;
   // Create nbVehicles vehicles
   for (let i = 0; i < nbVehicles; i++) {
     let vehicle = new Vehicle(random(width), random(height));
 
     vehicles.push(vehicle);
   }
+  */
+
+  creerLesCiblesPourFormerInitiales();
+  creerVehiclesPourFormerInitiales();
+}
+
+function creerLesCiblesPourFormerInitiales() {
+  // Pour le moment je crée deux cibles à des positions aléatoires et
+  // je les dessinerai juste avec des cercles, pour voir...
+  // Barre verticale gauche du M
+  cibles = [];
+  cibles.push(createVector(100, 100));
+  cibles.push(createVector(100, 150));
+  cibles.push(createVector(100, 200));
+  cibles.push(createVector(100, 250));
+  cibles.push(createVector(100, 300));
+
+  // Intérieur du M
+  cibles.push(createVector(150, 150));
+  cibles.push(createVector(200, 200));
+  cibles.push(createVector(250, 150));
+  cibles.push(createVector(300, 100));
+
+  // Barre verticale droite du M
+  cibles.push(createVector(300, 100));
+  cibles.push(createVector(300, 150));
+  cibles.push(createVector(300, 200));
+  cibles.push(createVector(300, 250));
+  cibles.push(createVector(300, 300));
+
+  // Le B !
+
+  // Barre verticale gauche du B
+  cibles.push(createVector(400, 100));
+  cibles.push(createVector(400, 150));
+  cibles.push(createVector(400, 200));
+  cibles.push(createVector(400, 250));
+  cibles.push(createVector(400, 300));
+
+  // Demi-cercle haut du B
+  cibles.push(createVector(450, 100));
+  cibles.push(createVector(500, 120));
+  cibles.push(createVector(520, 150));
+  cibles.push(createVector(500, 180));
+  cibles.push(createVector(450, 200));
+  cibles.push(createVector(500, 220));
+  cibles.push(createVector(520, 250));
+  cibles.push(createVector(500, 280));
+  cibles.push(createVector(450, 300));
+}
+
+function creerVehiclesPourFormerInitiales() {
+  for (let i = 0; i < cibles.length; i++) {
+    let vehicle = new Vehicle(random(width), random(height));
+    vehicles.push(vehicle);
+  }
+}
+
+function deplacerLesCibles() {
+  cibles.forEach((cible) => {
+    cible.x += random(-10, 10);
+    cible.y += random(-10, 10);
+  });
 }
 
 function draw() {
@@ -25,40 +91,92 @@ function draw() {
   noStroke();
   ellipse(target.x, target.y, 32);
 
-  vehicles.forEach((vehicle, index) => {
-    let forceArrive;
+  switch (demo) {
+    case "vehiculesFormentLettres":
+      /* POUR CIBLES QUI FORMENT LES INITIALES */
+      // Dessiner les cibles
 
-    if (index == 0) {
-      // C'est le 1er véhicule, il suit la cible/souris
-      forceArrive = vehicle.arrive(target);
-    } else {
-      // les véhicules suivants suivent le véhicule précédent
-      let vehiculePrecedent = vehicles[index - 1];
+      cibles.forEach((cible) => {
+        stroke("yellow");
+        noFill();
+        ellipse(cible.x, cible.y, 32);
+      });
 
-      //let pointDerriere = vehiculePrecedent.vel.copy().mult(-1);
-      //pointDerriere.setMag(70);
-      //pointDerriere.add(vehiculePrecedent.pos);
 
-      //on le dessine sous la forme d'un cercle vert
-      //fill("lightgreen");
-      //noStroke();
-      //circle(pointDerriere.x, pointDerriere.y, 10);
+      vehicles.forEach((vehicle, index) => {
+        let forceArrive;
+        let target = cibles[index];
 
-      forceArrive = vehicle.arrive(vehiculePrecedent.pos, 40);
-    }
+        forceArrive = vehicle.arrive(target);
+        // On applique la force au véhicule
+        vehicle.applyForce(forceArrive);
 
-    // On applique la force au véhicule
-    vehicle.applyForce(forceArrive);
+        // On met à jour la position et on dessine le véhicule
+        vehicle.update();
+        //vehicle.show();
+        // On dessine un cercle à la place du vehicule
+        fill("white");
+        stroke("blue");
+        circle(vehicle.pos.x, vehicle.pos.y, 40);
+      });
 
-    // On met à jour la position et on dessine le véhicule
-    vehicle.update();
-    vehicle.show();
-  });
 
+      // Toutes les 5 secondes, on déplace les cibles
+      if ((millis() > 5000) && (millis() < 10000)) {
+        deplacerLesCibles();
+      }
+      else {
+        // Au bout de 10s on réinitialise les cibles
+        // pour reformer les initiales
+        if ((millis() > 10000) && (millis() < 10100)) {
+          creerLesCiblesPourFormerInitiales();
+        }
+      }
+      break;
+    case "snake":
+      vehicles.forEach((vehicle, index) => {
+        let forceArrive;
+
+        if (index == 0) {
+          // C'est le 1er véhicule, il suit la cible/souris
+          forceArrive = vehicle.arrive(target);
+        } else {
+          // les véhicules suivants suivent le véhicule précédent
+          let vehiculePrecedent = vehicles[index - 1];
+
+          //let pointDerriere = vehiculePrecedent.vel.copy().mult(-1);
+          //pointDerriere.setMag(70);
+          //pointDerriere.add(vehiculePrecedent.pos);
+
+          //on le dessine sous la forme d'un cercle vert
+          //fill("lightgreen");
+          //noStroke();
+          //circle(pointDerriere.x, pointDerriere.y, 10);
+
+          forceArrive = vehicle.arrive(vehiculePrecedent.pos, 40);
+        }
+
+        // On applique la force au véhicule
+        vehicle.applyForce(forceArrive);
+
+        // On met à jour la position et on dessine le véhicule
+        vehicle.update();
+        vehicle.show();
+      });
+      break;
+  }
 }
 
 function keyPressed() {
-  if (key == 'd') {
-    Vehicle.debug = !Vehicle.debug;
+  switch(key) {
+    case 'd':
+      Vehicle.debug = !Vehicle.debug;
+      break;
+    case 'l':
+      demo = "vehiculesFormentLettres";
+      break;
+    case 's':
+      demo = "snake";
+      break;
   }
 }
--- 4-Wander/vehicle.js
+++ 4-Wander/vehicle.js
@@ -6,13 +6,15 @@ class Vehicle {
     this.maxSpeed = 4;
     this.maxForce = 0.2;
     this.r = 16;
-
+    // Pour comportement arrive
+    this.rayonZoneDeFreinage = 100;
 
     // pour comportement wander
     this.wanderTheta = PI / 2;
     this.displaceRange = 0.3;
 
     this.path = [];
+    this.nbMaxPointsChemin = 40;
   }
 
   wander() {
@@ -54,7 +56,7 @@ class Vehicle {
     circle(wanderPoint.x, wanderPoint.y, 16);
 
     // on dessine le vecteur desiredSpeed qui va du vaisseau au poibnt vert
-    //stroke(255);
+    stroke(255);
     line(this.pos.x, this.pos.y, wanderPoint.x, wanderPoint.y);
 
     // On a donc la vitesse désirée que l'on cherche qui est le vecteur
@@ -70,46 +72,104 @@ class Vehicle {
     this.wanderTheta += random(-this.displaceRange, this.displaceRange);
   }
 
-  evade(vehicle) {
-    let pursuit = this.pursue(vehicle);
-    pursuit.mult(-1);
-    return pursuit;
-  }
-
-  pursue(vehicle) {
-    let target = vehicle.pos.copy();
-    let prediction = vehicle.vel.copy();
-    prediction.mult(10);
-    target.add(prediction);
-    fill(0, 255, 0);
-    circle(target.x, target.y, 16);
-    return this.seek(target);
-  }
-
-  arrive(target) {
-    // 2nd argument true enables the arrival behavior
-    return this.seek(target, true);
-  }
-
-  flee(target) {
-    return this.seek(target).mult(-1);
-  }
-
-  seek(target, arrival = false) {
-    let force = p5.Vector.sub(target, this.pos);
-    let desiredSpeed = this.maxSpeed;
-    if (arrival) {
-      let slowRadius = 100;
-      let distance = force.mag();
-      if (distance < slowRadius) {
-        desiredSpeed = map(distance, 0, slowRadius, 0, this.maxSpeed);
+  /* Poursuite d'un point devant la target !
+      cette methode renvoie la force à appliquer au véhicule
+   */
+      pursue(target, evade = false) {
+        // TODO
+        // 1 - calcul de la position future de la cible
+        // on fait une copie de la position de la target
+        // 2 - On calcule un vecteur colinéaire au vecteur vitesse de la cible,
+        let prediction = target.vel.copy();
+        // et on le multiplie par 10 (10 frames)
+        // 3 - prediction dans 10 frames = 10 fois la longueur du vecteur
+    
+        // target.distancePrediction varie en fonction de
+        // la distance entre le véhicule et la cible
+        // plus la cible est loin, plus on prédit loin
+    
+        prediction.mult(target.distancePrediction);
+        // 4 - on positionne de la target au bout de ce vecteur
+        prediction.add(target.pos);
+    
+    
+        console.log("target distance prediction", target.distancePrediction)
+        // dessin du vecteur prediction
+        let v = p5.Vector.sub(prediction, target.pos);
+        this.drawVector(target.pos, v);
+    
+        // 2 - dessin d'un cercle vert de rayon 20 pour voir ce point
+        fill("green");
+        circle(prediction.x, prediction.y, 20);
+    
+        // 3 - appel à seek avec ce point comme cible 
+        let force = this.seek(prediction, evade);
+    
+        // n'oubliez pas, on renvoie la force à appliquer au véhicule !
+        return force;
       }
-    }
-    force.setMag(desiredSpeed);
-    force.sub(this.vel);
-    force.limit(this.maxForce);
-    return force;
-  }
+    
+      /* inverse de pursue
+         cette methode renvoie la force à appliquer au véhicule
+      */
+      evade(target) {
+        let evade = true;
+        return this.pursue(target, evade);
+      }
+    
+    
+      arrive(target, distanceVisee = 0) {
+        // 2nd argument true enables the arrival behavior
+        return this.seek(target, true, distanceVisee);
+      }
+    
+      flee(target) {
+        // recopier code de flee de l'exemple précédent
+      }
+    
+      seek(target, arrival = false, distanceVisee = 0) {
+        let force = p5.Vector.sub(target, this.pos);
+        let desiredSpeed = this.maxSpeed;
+    
+        if (arrival) {
+          // On définit un rayon de 100 pixels autour de la cible
+          // si la distance entre le véhicule courant et la cible
+          // est inférieure à ce rayon, on ralentit le véhicule
+          // desiredSpeed devient inversement proportionnelle à la distance
+          // si la distance est petite, force = grande
+          // Vous pourrez utiliser la fonction P5 
+          // distance = map(valeur, valeurMin, valeurMax, nouvelleValeurMin, nouvelleValeurMax)
+          // qui prend une valeur entre valeurMin et valeurMax et la transforme en une valeur
+          // entre nouvelleValeurMin et nouvelleValeurMax
+    
+          // TODO !
+          // Approche expérimentale
+          // on ajuste le rayon en fonction de la vitesse du véhicule
+          //console.log(this.vel.mag());
+          this.rayonZoneDeFreinage = this.vel.mag()*30;
+    
+          // 1 - dessiner le cercle de rayon 50 autour du vehicule
+          if (Vehicle.debug) {
+            stroke("white");
+            noFill();
+            circle(this.pos.x, this.pos.y, this.rayonZoneDeFreinage);
+          }
+    
+          // 2 - calcul de la distance entre la cible et le vehicule
+          let distance = p5.Vector.dist(this.pos, target);
+    
+          // 3 - si distance < rayon du cercle, alors on modifie desiredSPeed
+          // qui devient inversement proportionnelle à la distance.
+          // si d = rayon alors desiredSpeed = maxSpeed
+          // si d = 0 alors desiredSpeed = 0
+          desiredSpeed = map(distance, distanceVisee, this.rayonZoneDeFreinage + distanceVisee, 0, this.maxSpeed);
+        }
+    
+        force.setMag(desiredSpeed);
+        force.sub(this.vel);
+        force.limit(this.maxForce);
+        return force;
+      }
 
   applyForce(force) {
     this.acc.add(force);
@@ -125,7 +185,9 @@ class Vehicle {
     this.path.push(this.pos.copy());
 
     // si le tableau a plus de 50 éléments, on vire le plus ancien
-    // TODO
+    if (this.path.length > this.nbMaxPointsChemin) {
+      this.path.shift();
+    }
 
   }
 
